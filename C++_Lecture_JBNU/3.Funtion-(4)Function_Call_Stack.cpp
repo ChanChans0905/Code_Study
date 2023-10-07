b3cf32b8756aff96b76afe05d3767e81cc1347e3
Chan <chanchans0905@gmail.com>
2023-10-07 18:25:39 +0900

    2023.10.07

---
+++ C++_Lecture_JBNU/3.Funtion-(4)Function_Call_Stack.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// void static_local_increment()
+// {
+//     static int num = 1;
+//     std::cout << "num : " << num << std::endl;
+//     num++;
+//     std::cout << "num : " << num << std::endl;
+// }
+
+// int main()
+// {
+//     static_local_increment();
+//     static_local_increment();
+//     static_local_increment();
+// }
